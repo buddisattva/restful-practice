e209cda274acba8622650313181c6e5163851617
Wolfgang <wolfgang_xu@paradise-soft.com.tw>
2020-09-22 13:52:36 +0800

    refactor: remove unused route

--- app/main.go
+++ app/main.go
@@ -18,15 +18,8 @@ func main() {
 	goji.Get(albumRoutePrefix+"/:id", albumController.Show)
 
 	misc := new(controller.Misc)
-	// FIXME: unused
-	goji.Get("/", misc.Root)
 	// 404 handler
 	goji.NotFound(misc.NotFound)
 
-	// Call Serve() at the bottom of your main() function, and it'll take
-	// care of everything else for you, including binding to a socket (with
-	// automatic support for systemd and Einhorn) and supporting graceful
-	// shutdown on SIGINT. Serve() is appropriate for both development and
-	// production.
 	goji.Serve()
 }
